32b656c44935959a95b58e193fe48ccfaf5c70b7
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-24 19:04:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     index = Nokogiri::HTML(open(index_url))
-    index.css("div.student-card").each do |student|
+    index.css("div.roster-cards-container").each do |student|
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
